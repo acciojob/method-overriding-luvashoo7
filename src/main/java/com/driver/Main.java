aa3974f698540ddd03e5fc2ef886a5bd89ac23ba
Ashish <ashishrajstr@gmail.com>
2023-01-13 14:14:50 +0530

    Initial Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Contract(pure = true)
+        public static @NotNull String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
 }
+
